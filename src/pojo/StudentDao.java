3a90cbd0604df8e95488f0c50f41b132cffcb7b4
Karthik Kedam <karthikmsdian78@gmail.com>
2021-05-08 23:56:01 +0530

    intial commit

---
+++ src/pojo/StudentDao.java
@@ -0,0 +1,52 @@
+package pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDao {
+	
+	List<StudentPojo> allStudents;
+	
+	public StudentDao(){
+		allStudents = new ArrayList<StudentPojo>();
+		
+		StudentPojo s1 = new StudentPojo("mike", 20, "78337434738");
+		StudentPojo s2 = new StudentPojo("smith", 25, "8954875485");
+		StudentPojo s3 = new StudentPojo("adam", 30, "9847547478");
+		
+		allStudents.add(s1);
+		allStudents.add(s2);
+		allStudents.add(s3);
+	}
+	
+	public List<StudentPojo> getStudents() {
+		return allStudents;
+	}
+	
+	public StudentPojo addStudent(StudentPojo student) {
+		
+		allStudents.add(student);	
+		return student;
+	}
+	
+	public boolean deleteStudent(int studentId) {
+		for(StudentPojo student: allStudents) {
+			if(student.getStudentId() == studentId) {
+				allStudents.remove(student); 
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public StudentPojo updateStudent(StudentPojo updatedStudent) {
+		for(StudentPojo student: allStudents) {
+			if(student.getStudentId() == updatedStudent.getStudentId()) {
+				allStudents.set(allStudents.indexOf(student), updatedStudent);
+			}
+		}
+		return updatedStudent;
+	}
+		
+	
+}
---
+++ src/pojo/StudentPojo.java
@@ -0,0 +1,55 @@
+package pojo;
+
+public class StudentPojo {
+	
+	private int studentId;
+	private String studentName;
+	private int studentScore;
+	private String contact;
+	private static int prevStudentId = 0;
+	
+	public StudentPojo(String studentName, int studentScore, String contact) {
+		super();
+		studentId = ++prevStudentId;
+		this.studentName = studentName;
+		this.studentScore = studentScore;
+		this.contact = contact;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentScore() {
+		return studentScore;
+	}
+
+	public void setStudentScore(int studentScore) {
+		this.studentScore = studentScore;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public  int getStudentId() {
+		return studentId;
+	}
+	
+	@Override
+	public String toString() {
+		return "|studentId=" + studentId + ", studentName=" + studentName + ", studentMark=" + studentScore
+				+ ", studentContact=" + contact + "|";
+	}
+	
+	
+	
+}
